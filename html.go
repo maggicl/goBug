aec4f7d20d4319da7315774b76faf46c4669b796
Claudio Maggioni <maggioniclaudio1999@gmail.com>
2017-05-26 18:53:04 +0200

    Favicon serve statici

--- html.go
+++ html.go
@@ -17,6 +17,8 @@ func ServiHTML() {
 	}
 	http.HandleFunc("/tabella", handlerRoot("template/tabella.html"))
 	http.HandleFunc("/", handlerRoot("template/Interfaccia.html"))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":3000", nil)
 }
 
