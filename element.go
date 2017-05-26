db028ef08bb59c8ecfe4adf5dc42b014b4f0bf26
Giorgio Croci <Giorgio-Croci@users.noreply.github.com>
2017-05-26 14:45:02 +0200

    aggiunta paramentri e vario

--- element.go
+++ element.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 type Element struct { //struttura che contiene sia cibo sia amebe
-	IsFood bool
-	Health int
-	Age    int
+	IsFood     bool			//se il contenuto della cella è cibo
+	Health     int			//la sua vita
+	Age        int			//la sua età
+	Razza      string		//per distiguere amici da nemici
+	Evoluzione int			//se evolve in positivo avrà un bonus in attacco che viene sommato a Health
+	CostoMov	 int			//quanta energia spende per muoversi
+	CostoSex	 int			//quanto spende per riprodursi
 }
 
 func (e Element) String() string {
